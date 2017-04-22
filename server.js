ac463728a796e14b04d7a5520c114b6d94c867cf
Mai Hoang <mnhoang.80@gmail.com>
2017-04-22 10:58:29 -0700

    Modified file paths

--- server.js
+++ server.js
@@ -44,8 +44,12 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "view.html"));
 });
 
-app.get("/add", function(req, res) {
-  res.sendFile(path.join(__dirname, "add.html"));
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
 });
 
 // Search for Specific Character (or all characters) - provides JSON
